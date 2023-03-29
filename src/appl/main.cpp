b7e8a0c901c068b5251e87421083b335f7879d14
Stulov Tikhon <kudesnick@inbox.ru>
2023-03-29 22:49:51 +0300

    Add base data
    - minimal project
    - hardware documentation
    - RTT library
    - subrepo of tneo
    - subrepo of littlefs

---
+++ src/appl/main.cpp
@@ -0,0 +1,10 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void)
+{
+    printf("\033[31mC\033[32mO\033[33mL\033[34mO\033[35mR\033[42m \033[0m \033[36mT\033[37mE\033[30m\033[47mS\033[0mT\n"); // Color test
+
+    for(;;);
+}
---
+++ src/rtt/rtt_io.cpp
@@ -0,0 +1,209 @@
+
+#include "RTE_Components.h"
+#include CMSIS_device_header
+#include <stdio.h>
+
+#ifdef RTE_CMSIS_RTOS2
+    #include "cmsis_os2.h" // ARM::CMSIS:RTOS:Keil RTX
+#endif
+
+
+/// Консольный ввод/вывод
+#ifdef DEBUG
+    #define USR_PUT_RTT 1 ///< Вывод консоли в RTT
+    #define USR_PUT_ITM 0 ///< Вывод консоли в SWO
+    #define USR_GET_RTT 1 ///< Консольный ввод из RTT
+    #define USR_GET_ITM 0 ///< Консольный ввод из SWO
+#else
+    #define USR_PUT_RTT 0 ///< Вывод консоли в RTT
+    #define USR_PUT_ITM 0 ///< Вывод консоли в SWO
+    #define USR_GET_RTT 0 ///< Консольный ввод из RTT
+    #define USR_GET_ITM 0 ///< Консольный ввод из SWO
+#endif
+
+#if (USR_PUT_RTT == 1) && (USR_GET_RTT == 1)
+    #include "SEGGER_RTT.h"
+#endif
+
+
+#if (USR_PUT_ITM != 0) || (USR_GET_ITM != 0)
+    /* ITM registers */
+    #define ITM_PORT0_U8          (*reinterpret_cast<volatile uint8_t  *>(0xE0000000))
+    #define ITM_PORT0_U32         (*reinterpret_cast<volatile uint32_t *>(0xE0000000))
+    #define ITM_TER               (*reinterpret_cast<volatile uint32_t *>(0xE0000E00))
+    #define ITM_TCR               (*reinterpret_cast<volatile uint32_t *>(0xE0000E80))
+    #ifndef ITM_TCR_ITMENA_Msk
+    #define ITM_TCR_ITMENA_Msk    (1UL << 0)
+    #endif
+#endif
+
+
+#if defined(RTE_Compiler_IO_TTY) || defined(RTE_Compiler_IO_STDOUT) || defined(RTE_Compiler_IO_STDERR)
+#if defined(RTE_Compiler_IO_TTY_User) || defined(RTE_Compiler_IO_STDOUT_User) || defined(RTE_Compiler_IO_STDERR_User)
+
+extern "C" int usr_put_char(int);
+extern "C" void usr_put_str(const unsigned char *, uint32_t);
+
+#ifdef RTE_Compiler_IO_TTY_User
+    extern "C" void ttywrch(int);
+#endif
+#ifdef RTE_Compiler_IO_STDOUT_User
+    extern "C" int stdout_putchar(int);
+#endif
+#ifdef RTE_Compiler_IO_STDERR_User
+    extern "C" int stderr_putchar(int);
+#endif
+
+#endif
+#endif
+
+
+#if defined(RTE_Compiler_IO_TTY) || defined(RTE_Compiler_IO_STDIN)
+#if defined(RTE_Compiler_IO_TTY_User) || defined(RTE_Compiler_IO_STDIN_User)
+
+extern "C" int stdin_getchar(void);
+
+#endif
+#endif
+
+
+#if defined(RTE_Compiler_IO_TTY) || defined(RTE_Compiler_IO_STDOUT) || defined(RTE_Compiler_IO_STDERR)
+#if defined(RTE_Compiler_IO_TTY_User) || defined(RTE_Compiler_IO_STDOUT_User) || defined(RTE_Compiler_IO_STDERR_User)
+        
+int usr_put_char(int ch)
+{
+    #if (USR_PUT_RTT == 0) && (USR_PUT_ITM == 0)
+        (void)ch;
+        
+        return -1;
+    #else
+        
+        int result = -1;
+        
+        #if (USR_PUT_RTT != 0)
+            result = static_cast<int>(SEGGER_RTT_PutChar(0, static_cast<char>(ch)));
+        #endif
+        
+        #if (USR_PUT_ITM != 0)
+            if ((ITM_TCR & ITM_TCR_ITMENA_Msk) && /* ITM enabled */
+                (ITM_TER & (1UL << 0)        ))   /* ITM Port #0 enabled */
+            {
+                while (ITM_PORT0_U32 == 0);
+                __NOP();
+                ITM_PORT0_U8 = static_cast<uint8_t>(ch);
+            }
+            result = ch;
+        #endif
+        
+        return result;
+    #endif
+}
+
+void usr_put_str(const unsigned char *_buf, uint32_t _len)
+{
+    #if (USR_PUT_RTT == 0) && (USR_PUT_ITM == 0)
+        (void)_buf;
+        (void)_len;
+    #else
+        
+        #if (USR_PUT_RTT != 0)
+            SEGGER_RTT_Write(0, _buf, _len);
+        #endif
+        
+        #if (USR_PUT_ITM != 0)
+            for (; _len; _len--)
+            {
+                char ch = *_buf++;
+
+                if ((ITM_TCR & ITM_TCR_ITMENA_Msk) && /* ITM enabled */
+                    (ITM_TER & (1UL << 0)        ))   /* ITM Port #0 enabled */
+                {
+                    while (ITM_PORT0_U32 == 0);
+                    __NOP();
+                    ITM_PORT0_U8 = static_cast<uint8_t>(ch);
+                }
+            }
+        #endif
+    #endif
+}
+
+#ifdef RTE_Compiler_IO_TTY_User
+    void ttywrch(int ch)
+    {
+        usr_put_char(ch);
+    }
+#endif
+
+#ifdef RTE_Compiler_IO_STDOUT_User
+    int stdout_putchar(int ch)
+    {    
+        return usr_put_char(ch);
+    }
+#endif
+
+#ifdef RTE_Compiler_IO_STDERR_User
+    int stderr_putchar(int ch)
+    {
+        return usr_put_char(ch);
+    }
+#endif
+
+#endif
+#endif
+
+
+#if defined(RTE_Compiler_IO_TTY) || defined(RTE_Compiler_IO_STDIN)
+#if defined(RTE_Compiler_IO_TTY_User) || defined(RTE_Compiler_IO_STDIN_User)
+    
+#if (!defined(RTE_Compiler_IO_TTY_ITM)    && \
+     !defined(RTE_Compiler_IO_STDIN_ITM)  && \
+     !defined(RTE_Compiler_IO_STDOUT_ITM) && \
+     !defined(RTE_Compiler_IO_STDERR_ITM) && \
+     (USR_GET_ITM != 0))
+    
+    volatile int32_t ITM_RxBuffer = ITM_RXBUFFER_EMPTY;
+
+#endif
+
+int stdin_getchar(void)
+{
+    #if (USR_GET_RTT == 0) && (USR_GET_ITM == 0)
+        return -1;
+    #else
+        int result = -1;
+        
+        do
+        {
+
+        #if (USR_GET_RTT != 0)
+            result = SEGGER_RTT_GetKey();
+        #endif
+        
+        #if (USR_GET_ITM != 0)
+            if (result == -1)
+            {
+                extern volatile int32_t ITM_RxBuffer;
+                
+                if (ITM_RxBuffer != ITM_RXBUFFER_EMPTY)
+                {
+                    result = ITM_RxBuffer;
+                    ITM_RxBuffer = ITM_RXBUFFER_EMPTY;  /* ready for next character */
+                }
+            }
+        #endif
+        
+        #ifdef RTE_CMSIS_RTOS2
+            if (result == -1)
+            {
+                osThreadYield();
+            }
+        #endif
+        }
+        while (result == -1);
+        
+        return result;
+    #endif
+}
+
+#endif
+#endif
